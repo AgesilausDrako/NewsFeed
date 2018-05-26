7d4c3f655206a5bb88f6587e69ae4088d10cf8d9
AgesilausDrako <agesilaus@yahoo.com>
2018-05-26 19:51:47 -0400

    added initial components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import News from './News/News';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">My News</h1>
         </header>
+        <News />
       </div>
     );
   }
